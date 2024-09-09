9a7cdfb4d93c3ab7e4d71517a3339b2f2d6f5b6a
yuricamisa8 <francisco.cardoso94@aluno.ce.gov.br>
2024-09-09 08:47:57 -0300

    first commit

---
+++ atividade6.py
@@ -0,0 +1,4 @@
+# Crie um programa que receba dois nomes (primeiro nome e sobrenome) e exiba o nome completo.
+nome = str(input("coloque o seu nome"))
+sobrenome = str(input("coloque seu sobre nome"))
+print (nome+sobrenome)
